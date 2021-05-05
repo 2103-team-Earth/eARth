85741235df2904773e5e858be41a39eb115ab2c8
Morgan Mack <mackleemorgan@gmail.com>
2021-05-05 15:50:21 -0400

    FE: Via AR, renders a 3D box along with text

---
+++ js/HelloWorldSceneAR.js
@@ -0,0 +1,64 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import { StyleSheet } from 'react-native';
+
+import {
+  ViroARScene,
+  ViroText,
+  ViroConstants,
+  ViroBox,
+  ViroMaterials,
+} from 'react-viro';
+
+export default class HelloWorldSceneAR extends Component {
+  constructor() {
+    super();
+
+    // Set initial state here
+    this.state = {
+      text: 'Initializing AR...',
+    };
+
+    // bind 'this' to functions
+    this._onInitialized = this._onInitialized.bind(this);
+  }
+
+  render() {
+    return (
+      <ViroARScene onTrackingUpdated={this._onInitialized}>
+        <ViroText
+          text={this.state.text}
+          scale={[0.5, 0.5, 0.5]}
+          position={[0, 0, -1]}
+          style={styles.helloWorldTextStyle}
+        />
+        <ViroBox position={[0, -0.5, -1]} scale={[0.3, 0.3, 0.1]} />
+      </ViroARScene>
+    );
+  }
+
+  _onInitialized(state, reason) {
+    if (state == ViroConstants.TRACKING_NORMAL) {
+      this.setState({
+        // once initialized the below is rendered via AR
+        text: `What's in the box?!`,
+      });
+    } else if (state == ViroConstants.TRACKING_NONE) {
+      // Handle loss of tracking
+    }
+  }
+}
+
+var styles = StyleSheet.create({
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 30,
+    color: '#ffffff',
+    textAlignVertical: 'center',
+    textAlign: 'center',
+  },
+});
+
+module.exports = HelloWorldSceneAR;
